f82f783200530ae011d927697af6dd52efdbc99f
rainbowsdy <alexisrizza1@gmail.com>
2026-03-20 11:25:09 +0100

    ospfv2 implementé

--- ospfv2_routing.py
+++ ospfv2_routing.py
@@ -1,4 +1,3 @@
-# à dégager dès que l'on pourra appeler la fonction depuis le main
 import json
 with open('intent.json', 'r') as file:
     routing_data = json.load(file)
@@ -26,57 +25,29 @@ def Write_Ospf(router,AS_number):
         area_id = AS_number[1:]
 
         path = "config/R"+process_id+"_i"+process_id+"_startup-config.cfg"
-        line = " ipv6 ospf " + process_id + " area " + area_id +"\n"
 
         waitinglist = []
         for interface in routing_data["AS"][AS_number]["routers"][router]["interfaces"]:
-            waitinglist.append(interface)
+            waitinglist.append(interface) #mettre les interfaces en attente de config dans la liste
             
         
         with open(path, "r") as f:
                 config = f.readlines()
 
-        newconfig = []
-        i=0 #numero de la ligne
-        n=0 #numero du routeur
-        verif = 0 #on vérifie si on a bien tout écrit
-        while i < len(config):
-            #on détecte l'interface qui nous intéresse
-            if n<len(waitinglist) and config[i] == "interface " + waitinglist[n] + "\n":
-                while config[i]!="!\n":
-                    newconfig.append(config[i])
-                    i+=1
-                #on ajoute la ligne de configuration
-                if config[i-1].startswith(f" ipv6 address 2001:{AS_number}") or config[i-1].startswith(f" ipv6 address 2001:DB8") : 
-                    newconfig.append(line)
-                n+=1
-                verif+=1
-                if not config[i+1].startswith("interface"):
-                    newconfig.append("!\n")
-                    newconfig.append("router ospf "+process_id+"\n")
-                    newconfig.append("!\n")
-
-                    i+=1
-                    n=0
-            else : 
-                newconfig.append(config[i])
-                i+=1
-
-            # Si on a été bloqué par l'absence d'une interface, on recommence sans
-            if i == len(config)-1 and verif != len(waitinglist):
-                print(f"Warning, interface {waitinglist[verif]} is missing for router R{process_id} !")
-                i=0
-                waitinglist.pop(verif)
-                newconfig=[]
         j = 0
         NewNewConfig=[]
-        while j < len(newconfig) : 
-            NewNewConfig.append(newconfig[j])
-            if newconfig[j].startswith("no ip http secure-server"):
+        while j < len(config) :
+            NewNewConfig.append(config[j])
+            if config[j].startswith("no ip http secure-server"):
                 NewNewConfig.append("!\n")
-                NewNewConfig.append("ipv6 router ospf "+process_id+"\n")
-                NewNewConfig.append(f" router-id {process_id}.{process_id}.{process_id}.{process_id}\n")
-                NewNewConfig.append(" redistribute bgp " + AS_number + "\n")
+
+                NewNewConfig.append(f"router ospf {process_id}\n")
+                NewNewConfig.append(f" network 192.168.0.0 0.0.255.255 area {area_id}\n")
+                NewNewConfig.append(f" passive-interface default\n")
+                for interfaces in waitinglist:
+                    NewNewConfig.append(f" no passive-interface {interfaces}\n")
+                NewNewConfig.append(f" redistribute bgp {AS_number} route-map FROM-PROVIDER-IN\n") #---------------------------------DEMANDER AU PROF
+                NewNewConfig.append(f" redistribute bgp {AS_number} route-map FROM-CUSTOMER-IN\n")
             j+=1
 
         #on ecrit la nouvelle config
